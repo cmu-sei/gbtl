d33c4a109a501e5476c72dcafa639bee6deceae6
Peter Zhang <yz79@indiana.edu>
2016-04-01 15:57:58 -0400

    negate might not actually work without populating the whole array

--- src/test/operations_test/utility_test.cu
+++ src/test/operations_test/utility_test.cu
@@ -7,11 +7,14 @@ int main(){
     thrust::device_vector<int> b(12,2);
     graphblas::filter(a, 32, b, 12);
 
-    graphblas::backend::detail::make_col_index_iterator(4,4);
-    graphblas::backend::detail::make_row_index_iterator(4,4);
+    auto r=graphblas::backend::detail::make_col_index_iterator(4,4);
+    auto c=graphblas::backend::detail::make_row_index_iterator(4,4);
 
     thrust::device_vector<int> e(12,1);
     thrust::device_vector<int> d(12,2);
 
-    graphblas::backend::detail::make_val_iterator(e.begin(), d.begin(), 4,4, 16, 4, graphblas::ArithmeticSemiring<int>());
+    auto v=graphblas::backend::detail::make_val_iterator(e.begin(), d.begin(), 4,4, 16, 4, graphblas::ArithmeticSemiring<int>());
+
+    cusp::coo_matrix_view <decltype(r), decltype(c), decltype(v)>
+        vi(4,4,16,r,c,v);
 };
