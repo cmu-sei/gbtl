534524e2c66410591a6c80a46569c50b70db3471
Scott McMillan <smcmillan@sei.cmu.edu>
2018-05-01 12:44:58 -0400

    Moving extract API logging messages to frontend operations.hpp
    Removing dead code (or unnecessary code) in sparse_extract. Consolidating extract tests into one file.
    
    Minor formatting/style changes

--- src/graphblas/system/sequential/sparse_extract.hpp
+++ src/graphblas/system/sequential/sparse_extract.hpp
@@ -24,6 +24,7 @@
 #include <utility>
 #include <vector>
 #include <iterator>
+#include <type_traits>
 #include <iostream>
 
 #include <graphblas/detail/logging.h>
@@ -43,12 +44,24 @@ namespace GraphBLAS
     {
         //**********************************************************************
 
-        template < typename CScalarT,
-                   typename AScalarT,
-                   typename IteratorT>
+        /**
+         * Extracts a series of values from the vector based on the passed in
+         * indices.
+         * @tparam CScalarT  The type of the output scalar.
+         * @tparam AScalarT  The type of the input scalar.
+         * @tparam SequenceT A random access iterator into a container of indices
+         *
+         * @param vec_dest The output vector.
+         * @param vec_src The input vector.
+         * @param begin   Iterator at begining of sequence of indices to extract.
+         * @param end     Iterator at end of sequence of indices to extract.
+         */
+        template<typename CScalarT,
+                 typename AScalarT,
+                 typename IteratorT>
         void vectorExtract(
-                std::vector< std::tuple<IndexType, CScalarT> >  &vec_dest,
-                std::vector< std::tuple<IndexType, AScalarT> > const &vec_src,
+                std::vector<std::tuple<IndexType, CScalarT> >       &vec_dest,
+                std::vector<std::tuple<IndexType, AScalarT> > const &vec_src,
                 IteratorT           begin,
                 IteratorT           end)
         {
@@ -78,46 +91,6 @@ namespace GraphBLAS
             }
         }
 
-        /**
-         * Extracts a series of values from the vector based on the passed in
-         * indices.
-         * @tparam CScalarT The type of the output scalar.
-         * @tparam AScalarT The type of the input scalar.
-         * @param vec_dest The output vector.
-         * @param vec_src The input vector.
-         * @param indices The indices to extract.
-         */
-        template < typename CScalarT,
-                   typename AScalarT,
-                   typename SequenceT>
-        void vectorExtract(
-            std::vector< std::tuple<IndexType, CScalarT> >  &vec_dest,
-            std::vector< std::tuple<IndexType, AScalarT> > const &vec_src,
-            SequenceT const & indices)
-        {
-            // This is expensive but the indices can be duplicates and
-            // out of order.
-
-            vec_dest.clear();
-            for (IndexType out_idx = 0; out_idx < indices.size(); ++out_idx)
-            {
-                IndexType wanted_idx = indices[out_idx];
-                IndexType tmp_idx;
-                AScalarT tmp_value;
-
-                // Search through the outputs find one that matches.
-                auto A_it = vec_src.begin();
-                increment_while_below(A_it, vec_src.end(), wanted_idx);
-                if (A_it != vec_src.end())
-                {
-                    std::tie(tmp_idx, tmp_value) = *A_it;
-                    if (tmp_idx == wanted_idx)
-                        vec_dest.push_back(
-                                std::make_tuple(out_idx,
-                                                static_cast<CScalarT>(tmp_value)));
-                }
-            }
-        }
 
         // *******************************************************************
         template<typename CScalarT,
@@ -225,7 +198,7 @@ namespace GraphBLAS
         }
 
         //********************************************************************
-        template < typename WScalarT, typename AScalarT, typename IteratorT>
+        template <typename WScalarT, typename AScalarT, typename IteratorT>
         void extractColumn(
             std::vector< std::tuple<IndexType, WScalarT> >         &vec_dest,
             LilSparseMatrix<AScalarT>                       const  &A,
@@ -271,7 +244,7 @@ namespace GraphBLAS
 
         //********************************************************************
         // Extract a row of a matrix using TransposeView
-        template < typename WScalarT, typename AMatrixT, typename IteratorT>
+        template <typename WScalarT, typename AMatrixT, typename IteratorT>
         void extractColumn(
             std::vector< std::tuple<IndexType, WScalarT> >  &vec_dest,
             backend::TransposeView<AMatrixT> const          &Atrans,
@@ -288,7 +261,7 @@ namespace GraphBLAS
             auto row(Atrans.getCol(col_index));
 
             // Walk the 'row'
-            /// @todo Perf. can be improved for in order row_indices with "continuation"
+            /// @todo Perf. can be improved for 'in order' row_indices with "continuation"
             IndexType out_row_index = 0;
 
             //for (IndexType idx = 0; idx < row_indices.size(); ++idx)
@@ -309,43 +282,6 @@ namespace GraphBLAS
             } // for
         }
 
-#if 0
-        //********************************************************************
-        // Extract a row of a matrix using TransposeView
-        template < typename WScalarT, typename AMatrixT, typename IteratorT>
-        void extractColumn(
-            std::vector< std::tuple<IndexType, WScalarT> >        &vec_dest,
-            backend::TransposeView<AMatrixT>                const &Atrans,
-            IndexSequence                                  const &row_indices,
-            IndexType                                              col_index)
-        {
-            // Walk the row, extracting the cell if it exists and is in row_indices
-            typedef typename AMatrixT::ScalarType AScalarType;
-            typedef std::vector<std::tuple<IndexType,AScalarType> > ARowType;
-
-            vec_dest.clear();
-
-            ARowType row(Atrans.getCol(col_index));
-
-            // Walk the 'row'
-            /// @todo Perf. can be improved for in order row_indices with "continuation"
-            for (IndexType idx = 0; idx < row_indices.size(); ++idx)
-            {
-                auto row_it = row.begin();
-                while (row_it != row.end())
-                {
-                    IndexType in_row_index(std::get<0>(*row_it));
-                    if (in_row_index == row_indices[idx])
-                    {
-                        vec_dest.push_back(
-                            std::make_tuple(idx,
-                                            static_cast<WScalarT>(std::get<1>(*row_it))));
-                    }
-                    ++row_it;
-                }
-            } // for
-        }
-#endif
         //**********************************************************************
         //**********************************************************************
         //**********************************************************************
@@ -370,15 +306,6 @@ namespace GraphBLAS
                      SequenceT          const &indices,
                      bool                      replace_flag = false)
         {
-            GRB_LOG_FN_BEGIN("SEQUENTIAL extract - 4.3.6.1 - standard vector variant");
-
-            GRB_LOG_VERBOSE("w:    " << w);
-            GRB_LOG_VERBOSE("mask: " << mask);
-            GRB_LOG_VERBOSE("u:    " << u);
-            GRB_LOG_VERBOSE_ACCUM(accum);
-            GRB_LOG_VERBOSE("Indices: " << indices);
-            GRB_LOG_VERBOSE_REPLACE(replace_flag);
-
             check_index_array_content(indices, u.size(),
                                       "extract(std vec): indices >= u.size");
 
@@ -390,13 +317,18 @@ namespace GraphBLAS
             typedef typename UVectorT::ScalarType UScalarType;
             std::vector<std::tuple<IndexType, UScalarType> > t;
             auto u_contents(u.getContents());
-            vectorExtract(t, u_contents, indices);
+            vectorExtract(t, u_contents, indices.begin(), indices.end());
 
             GRB_LOG_VERBOSE("T: " << t);
 
             // =================================================================
             // Accumulate into Z
-            std::vector<std::tuple<IndexType, WScalarType> > z;
+            typedef typename std::conditional<
+                std::is_same<AccumT, NoAccumulate>::value,
+                UScalarType,
+                typename AccumT::result_type>::type  ZScalarType;
+
+            std::vector<std::tuple<IndexType, ZScalarType> > z;
             ewise_or_opt_accum_1D(z, w, t, accum);
 
             GRB_LOG_VERBOSE("Z: " << z);
@@ -406,7 +338,6 @@ namespace GraphBLAS
             write_with_opt_mask_1D(w, z, mask, replace_flag);
 
             GRB_LOG_VERBOSE("W (Result): " << w);
-            GRB_LOG_FN_END("SEQUENTIAL extract - 4.3.6.1 - standard vector variant");
         };
 
         //**********************************************************************
@@ -430,28 +361,15 @@ namespace GraphBLAS
                      ColSequenceT       const   &col_indices,
                      bool                        replace_flag = false)
         {
-            GRB_LOG_FN_BEGIN("SEQUENTIAL extract - 4.3.6.2 - standard matrix variant");
-
-            GRB_LOG_VERBOSE("C: " << C);
-            GRB_LOG_VERBOSE("Mask: " << Mask);
-            GRB_LOG_VERBOSE_ACCUM(accum);
-            GRB_LOG_VERBOSE("A: " << A);
-            GRB_LOG_VERBOSE("row_Indices: " << row_indices);
-            GRB_LOG_VERBOSE("col_Indices: " << col_indices);
-            GRB_LOG_VERBOSE_REPLACE(replace_flag);
-
             check_index_array_content(row_indices, A.nrows(),
                                       "extract(std mat): row_indices >= A.nrows");
             check_index_array_content(col_indices, A.ncols(),
                                       "extract(std mat): col_indices >= A.ncols");
 
-            typedef typename CMatrixT::ScalarType CScalarType;
-            typedef std::vector<std::tuple<IndexType,CScalarType> > CColType;
-
-
             // =================================================================
             // Extract to T
-            LilSparseMatrix<CScalarType> T(C.nrows(), C.ncols());
+            typedef typename AMatrixT::ScalarType AScalarType;
+            LilSparseMatrix<AScalarType> T(C.nrows(), C.ncols());
             matrixExtract(T, A,
                           setupIndices(row_indices, A.nrows()),
                           setupIndices(col_indices, A.ncols()));
@@ -460,8 +378,12 @@ namespace GraphBLAS
 
             // =================================================================
             // Accumulate into Z
+            typedef typename std::conditional<
+                std::is_same<AccumT, NoAccumulate>::value,
+                AScalarType,
+                typename AccumT::result_type>::type  ZScalarType;
 
-            LilSparseMatrix<CScalarType> Z(C.nrows(), C.ncols());
+            LilSparseMatrix<ZScalarType> Z(C.nrows(), C.ncols());
             ewise_or_opt_accum(Z, C, T, accum);
 
             GRB_LOG_VERBOSE("Z: " << Z);
@@ -471,7 +393,6 @@ namespace GraphBLAS
             write_with_opt_mask(C, Z, Mask, replace_flag);
 
             GRB_LOG_VERBOSE("C (Result): " << C);
-            GRB_LOG_FN_END("SEQUENTIAL extract - 4.3.6.2 - standard matrix variant");
         };
 
 
@@ -497,28 +418,14 @@ namespace GraphBLAS
                      IndexType                 col_index,
                      bool                      replace_flag = false)
         {
-            GRB_LOG_FN_BEGIN("SEQUENTIAL extract - 4.3.6.3 - column (and row) variant");
-
-            GRB_LOG_VERBOSE("w:    " << w);
-            GRB_LOG_VERBOSE("mask: " << mask);
-            GRB_LOG_VERBOSE_ACCUM(accum);
-            GRB_LOG_VERBOSE("A:    " << A);
-            GRB_LOG_VERBOSE("row_Indices: " << row_indices);
-            GRB_LOG_VERBOSE("col_index:    " << col_index);
-            GRB_LOG_VERBOSE_REPLACE(replace_flag);
-
             check_index_array_content(row_indices, A.nrows(),
                                       "extract(col): row_indices >= A.nrows");
 
-            // Should explicitly define the vector type, or just piggy
-            // back on WVectorT?
-            //WVectorT wTmp;
-            typedef typename WVectorT::ScalarType WScalarType;
-            typedef std::vector<std::tuple<IndexType, WScalarType>> WVectorType;
-
             // =================================================================
             // Extract to T
-            WVectorType t;
+            typedef typename AMatrixT::ScalarType AScalarType;
+            typedef std::vector<std::tuple<IndexType, AScalarType>> TVectorType;
+            TVectorType t;
 
             auto seq = setupIndices(row_indices, w.size());
             extractColumn(t, A, seq.begin(), seq.end(), col_index);
@@ -527,7 +434,12 @@ namespace GraphBLAS
 
             // =================================================================
             // Accumulate into Z
-            std::vector<std::tuple<IndexType, WScalarType> > z;
+            typedef typename std::conditional<
+                std::is_same<AccumT, NoAccumulate>::value,
+                AScalarType,
+                typename AccumT::result_type>::type  ZScalarType;
+
+            std::vector<std::tuple<IndexType, ZScalarType> > z;
             ewise_or_opt_accum_1D(z, w, t, accum);
 
             GRB_LOG_VERBOSE("z: " << z);
@@ -537,7 +449,6 @@ namespace GraphBLAS
             write_with_opt_mask_1D(w, z, mask, replace_flag);
 
             GRB_LOG_VERBOSE("w (Result): " << w);
-            GRB_LOG_FN_END("SEQUENTIAL extract - 4.3.6.3 - column (and row) variant");
         }
     }
 }
