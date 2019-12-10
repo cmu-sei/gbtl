4ac887cffd521d16ac1de6d6a52f322b75385046
Scott McMillan <smcmillan@sei.cmu.edu>
2019-12-10 08:58:01 -0500

    Changing some checks to take 'reference to const' instead of pass by value.

--- src/graphblas/detail/checks.hpp
+++ src/graphblas/detail/checks.hpp
@@ -91,13 +91,13 @@ namespace GraphBLAS
     // ================================================
 
     template <typename M1, typename M2>
-    void check_nrows_nrows(M1 m1, M2 m2, const std::string &msg)
+    void check_nrows_nrows(M1 const &m1, M2 const &m2, const std::string &msg)
     {
         check_val_equals(m1.nrows(), m2.nrows(), "nrows != nrows", msg);
     }
 
     template <typename M1>
-    void check_nrows_nrows(M1 m1, NoMask mask, const std::string &msg)
+    void check_nrows_nrows(M1 const & m1, NoMask const &mask, const std::string &msg)
     {
         // No op
     }
@@ -105,13 +105,13 @@ namespace GraphBLAS
     // ================================================
 
     template <typename M1, typename M2>
-    void check_ncols_ncols(M1 m1, M2 m2, const std::string &msg)
+    void check_ncols_ncols(M1 const &m1, M2 const &m2, const std::string &msg)
     {
         check_val_equals(m1.ncols(), m2.ncols(), "ncols != ncols", msg);
     }
 
     template <typename M1>
-    void check_ncols_ncols(M1 m1, NoMask mask, const std::string &msg)
+    void check_ncols_ncols(M1 const &m1, NoMask const &mask, const std::string &msg)
     {
         // No op
     }
@@ -119,19 +119,19 @@ namespace GraphBLAS
     // ================================================
 
     template <typename M1, typename M2>
-    void check_ncols_nrows(M1 m1, M2 m2, const std::string &msg)
+    void check_ncols_nrows(M1 const &m1, M2 const &m2, const std::string &msg)
     {
         check_val_equals(m1.ncols(), m2.nrows(), "ncols != nrows", msg);
     };
 
     template <typename M>
-    void check_ncols_nrows(M m, NoMask mask, const std::string &msg)
+    void check_ncols_nrows(M const &m, NoMask const &mask, const std::string &msg)
     {
         // No op
     };
 
     template <typename M>
-    void check_ncols_nrows(NoMask mask, M m, const std::string &msg)
+    void check_ncols_nrows(NoMask const &mask, M const &m, const std::string &msg)
     {
         // No op
     };
