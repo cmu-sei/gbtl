72954863fc060afcc049c69da8718bbe88088b9c
Scott McMillan <smcmillan@sei.cmu.edu>
2021-08-11 16:04:03 -0700

    Code cleanup. Recording performance. Setting default build type to Release.

--- src/graphblas/platforms/optimized_sequential/sparse_mxv.hpp
+++ src/graphblas/platforms/optimized_sequential/sparse_mxv.hpp
@@ -57,69 +57,7 @@ namespace grb
                         UVectorT     const &u,
                         OutputControlEnum  outp)
         {
-            mxv_dot_nomask_noaccum(w, op, A, u);
-        }
-
-        template<typename WVectorT,
-                 typename AccumT,
-                 typename SemiringT,
-                 typename AScalarT,
-                 typename UVectorT>
-        inline void mxv(WVectorT           &w,
-                        NoMask       const &mask,
-                        AccumT       const &accum,
-                        SemiringT           op,
-                        LilSparseMatrix<AScalarT>     const &A,
-                        UVectorT     const &u,
-                        OutputControlEnum  outp)
-        {
-            mxv_dot_nomask_accum(w, accum, op, A, u);
-        }
-
-        template<typename WVectorT,
-                 typename MaskT,
-                 typename SemiringT,
-                 typename AScalarT,
-                 typename UVectorT>
-        inline void mxv(WVectorT           &w,
-                        MaskT        const &mask,
-                        NoAccumulate const &accum,
-                        SemiringT           op,
-                        LilSparseMatrix<AScalarT>     const &A,
-                        UVectorT     const &u,
-                        OutputControlEnum   outp)
-        {
-            mxv_dot_mask_noaccum(w, mask, op, A, u, outp);
-        }
-
-        template<typename WVectorT,
-                 typename MaskT,
-                 typename AccumT,
-                 typename SemiringT,
-                 typename AScalarT,
-                 typename UVectorT>
-        inline void mxv(WVectorT          &w,
-                        MaskT       const &mask,
-                        AccumT      const &accum,
-                        SemiringT          op,
-                        LilSparseMatrix<AScalarT>     const &A,
-                        UVectorT    const &u,
-                        OutputControlEnum  outp)
-        {
-            mxv_dot_mask_accum(w, mask, accum, op, A, u, outp);
-        }
-
-        //**********************************************************************
-        //**********************************************************************
-        template<typename WVectorT,
-                 typename SemiringT,
-                 typename AMatrixT,
-                 typename UVectorT>
-        inline void mxv_dot_nomask_noaccum(WVectorT          &w,
-                                           SemiringT          op,
-                                           AMatrixT    const &A,
-                                           UVectorT    const &u)
-        {
+            //mxv_dot_nomask_noaccum(w, op, A, u);
             GRB_LOG_VERBOSE("w := A +.* u");
             w.clear();
 
@@ -129,7 +67,6 @@ namespace grb
 
             if ((A.nvals() > 0) && (u.nvals() > 0))
             {
-                //auto u_contents(u.getContents());
                 for (IndexType row_idx = 0; row_idx < w.size(); ++row_idx)
                 {
                     if ( !A[row_idx].empty() )
@@ -145,18 +82,22 @@ namespace grb
         }
 
         //**********************************************************************
+        /// Implementation for 4.3.3 mxv: w + A * u
         //**********************************************************************
         template<typename WVectorT,
                  typename AccumT,
                  typename SemiringT,
-                 typename AMatrixT,
+                 typename AScalarT,
                  typename UVectorT>
-        inline void mxv_dot_nomask_accum(WVectorT          &w,
-                                         AccumT      const &accum,
-                                         SemiringT          op,
-                                         AMatrixT    const &A,
-                                         UVectorT    const &u)
+        inline void mxv(WVectorT           &w,
+                        NoMask       const &mask,
+                        AccumT       const &accum,
+                        SemiringT           op,
+                        LilSparseMatrix<AScalarT>     const &A,
+                        UVectorT     const &u,
+                        OutputControlEnum  outp)
         {
+            //mxv_dot_nomask_accum(w, accum, op, A, u);
             GRB_LOG_VERBOSE("w := w + (A +.* u)");
 
             // =================================================================
@@ -165,7 +106,6 @@ namespace grb
 
             if ((A.nvals() > 0) && (u.nvals() > 0))
             {
-                //auto u_contents(u.getContents());
                 for (IndexType row_idx = 0; row_idx < w.size(); ++row_idx)
                 {
                     if ( !A[row_idx].empty() )
@@ -175,9 +115,10 @@ namespace grb
                         {
                             if (w.hasElementNoCheck(row_idx))
                             {
-                                w.setElement(row_idx,
-                                             accum(w.extractElementNoCheck(row_idx),
-                                                   t_val));
+                                w.setElement(
+                                    row_idx,
+                                    accum(w.extractElementNoCheck(row_idx),
+                                          t_val));
                             }
                             else
                             {
@@ -190,19 +131,22 @@ namespace grb
         }
 
         //**********************************************************************
+        /// Implementation for 4.3.3 mxv: <m,r>(A * u)
         //**********************************************************************
         template<typename WVectorT,
                  typename MaskT,
                  typename SemiringT,
-                 typename AMatrixT,
+                 typename AScalarT,
                  typename UVectorT>
-        inline void mxv_dot_mask_noaccum(WVectorT          &w,
-                                         MaskT       const &mask,
-                                         SemiringT          op,
-                                         AMatrixT    const &A,
-                                         UVectorT    const &u,
-                                         OutputControlEnum  outp)
+        inline void mxv(WVectorT           &w,
+                        MaskT        const &mask,
+                        NoAccumulate const &accum,
+                        SemiringT           op,
+                        LilSparseMatrix<AScalarT>     const &A,
+                        UVectorT     const &u,
+                        OutputControlEnum   outp)
         {
+            //mxv_dot_mask_noaccum(w, mask, op, A, u, outp);
             GRB_LOG_VERBOSE("w<M,r> := (A +.* u)");
 
             // =================================================================
@@ -215,7 +159,6 @@ namespace grb
 
             if ((A.nvals() > 0) && (u.nvals() > 0))
             {
-                //auto u_contents(u.getContents());
                 for (IndexType row_idx = 0; row_idx < w.size(); ++row_idx)
                 {
                     bool element_set = false;
@@ -245,29 +188,28 @@ namespace grb
         }
 
         //**********************************************************************
+        /// Implementation for 4.3.3 mxv: <m,r>(w + A * u)
         //**********************************************************************
         template<typename WVectorT,
                  typename MaskT,
                  typename AccumT,
                  typename SemiringT,
-                 typename AMatrixT,
+                 typename AScalarT,
                  typename UVectorT>
-        inline void mxv_dot_mask_accum(WVectorT          &w,
-                                       MaskT       const &mask,
-                                       AccumT      const &accum,
-                                       SemiringT          op,
-                                       AMatrixT    const &A,
-                                       UVectorT    const &u,
-                                       OutputControlEnum  outp)
+        inline void mxv(WVectorT          &w,
+                        MaskT       const &mask,
+                        AccumT      const &accum,
+                        SemiringT          op,
+                        LilSparseMatrix<AScalarT>     const &A,
+                        UVectorT    const &u,
+                        OutputControlEnum  outp)
         {
+            //mxv_dot_mask_accum(w, mask, accum, op, A, u, outp);
             GRB_LOG_VERBOSE("w<M,r> := w + (A +.* u)");
 
             // =================================================================
             // Do the basic dot-product work with the semi-ring.
             using TScalarType = typename SemiringT::result_type;
-#if defined(ORIGINAL_CODE)
-            std::vector<std::tuple<IndexType, TScalarType> > t;
-#endif
 
             // =================================================================
             // Accumulate into Z
@@ -278,25 +220,16 @@ namespace grb
 
             if ((A.nvals() > 0) && (u.nvals() > 0))
             {
-                //auto u_contents(u.getContents());
                 for (IndexType row_idx = 0; row_idx < w.size(); ++row_idx)
                 {
-#if defined(ORIGINAL_CODE)
-                    if ( !A[row_idx].empty() )
-#else
                     if (check_mask_1D(mask, row_idx) && !A[row_idx].empty())
-#endif
                     {
                         TScalarType t_val;
                         if (dot_sparse_dense(t_val, A[row_idx], u, op))
                         {
-#if defined(ORIGINAL_CODE)
-                            t.emplace_back(row_idx, t_val);
-#else
                             if (std::is_same_v<AccumT, grb::NoAccumulate>)
                             {
                                 z.emplace_back(row_idx, (ZScalarType)t_val);
-                                //std::cout << row_idx << ": no accum t\n";
                             }
                             else
                             {
@@ -305,31 +238,24 @@ namespace grb
                                     z.emplace_back(row_idx,
                                                    accum(w.get_vals()[row_idx],
                                                          t_val));
-                                    //std::cout << row_idx << ": accum(w, t)\n";
                                 }
                                 else
                                 {
                                     z.emplace_back(row_idx, (ZScalarType)t_val);
-                                    //std::cout << row_idx << ": accum(no w, t)\n";
                                 }
                             }
                         }
                     }
                     else
                     {
-                        if ((!std::is_same_v<AccumT, grb::NoAccumulate>) && (w.get_bitmap()[row_idx]))
+                        if ((!std::is_same_v<AccumT, grb::NoAccumulate>) &&
+                            (w.get_bitmap()[row_idx]))
                         {
                             z.emplace_back(row_idx, w.get_vals()[row_idx]);
-                            //std::cout << row_idx << ": accum(w, no t)\n";
-#endif
                         }
                     }
                 }
             }
-
-#if defined(ORIGINAL_CODE)
-            ewise_or_opt_accum_1D(z, w, t, accum);
-#endif
 
             // =================================================================
             // Copy Z into the final output, w, considering mask and replace/merge
@@ -341,7 +267,7 @@ namespace grb
         //**********************************************************************
 
         //**********************************************************************
-        /// Implementation of 4.3.3 mxv: A' * u
+        /// Implementation of 4.3.3 mxv: w<m,r> = w + A' * u
         //**********************************************************************
         template<typename WVectorT,
                  typename MaskT,
