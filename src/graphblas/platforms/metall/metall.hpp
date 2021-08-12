718ae2d3749cdc6e35d6a50a15b40c8852169a5d
Scott McMillan <smcmillan@sei.cmu.edu>
2021-08-12 13:00:07 -0700

    Starting a new backend for Metall work.

---
+++ src/graphblas/platforms/metall/metall.hpp
@@ -0,0 +1,34 @@
+/*
+ * GraphBLAS Template Library (GBTL), Version 3.0
+ *
+ * Copyright 2020 Carnegie Mellon University, Battelle Memorial Institute, and
+ * Authors.
+ *
+ * THIS MATERIAL WAS PREPARED AS AN ACCOUNT OF WORK SPONSORED BY AN AGENCY OF
+ * THE UNITED STATES GOVERNMENT.  NEITHER THE UNITED STATES GOVERNMENT NOR THE
+ * UNITED STATES DEPARTMENT OF ENERGY, NOR THE UNITED STATES DEPARTMENT OF
+ * DEFENSE, NOR CARNEGIE MELLON UNIVERSITY, NOR BATTELLE, NOR ANY OF THEIR
+ * EMPLOYEES, NOR ANY JURISDICTION OR ORGANIZATION THAT HAS COOPERATED IN THE
+ * DEVELOPMENT OF THESE MATERIALS, MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
+ * ASSUMES ANY LEGAL LIABILITY OR RESPONSIBILITY FOR THE ACCURACY, COMPLETENESS,
+ * OR USEFULNESS OR ANY INFORMATION, APPARATUS, PRODUCT, SOFTWARE, OR PROCESS
+ * DISCLOSED, OR REPRESENTS THAT ITS USE WOULD NOT INFRINGE PRIVATELY OWNED
+ * RIGHTS.
+ *
+ * Released under a BSD-style license, please see LICENSE file or contact
+ * permission@sei.cmu.edu for full terms.
+ *
+ * [DISTRIBUTION STATEMENT A] This material has been approved for public release
+ * and unlimited distribution.  Please see Copyright notice for non-US
+ * Government use and distribution.
+ *
+ * DM20-0442
+ */
+
+#pragma once
+
+#include <graphblas/platforms/metall/BitmapSparseVector.hpp>
+#include <graphblas/platforms/metall/LilSparseMatrix.hpp>
+#include <graphblas/platforms/metall/param_unpack.hpp>
+
+#include <graphblas/platforms/metall/operations.hpp>
